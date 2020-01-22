320a02881642e4e10f65282aeae115ae2f48a648
Miguel Lima <miguelsousalima@hotmail.com>
2020-01-22 06:36:42 -0300

    Subindo Projeto Mirror Fashion

---
+++ js/produto.js
@@ -0,0 +1,8 @@
+var inputTamanho = document.querySelector('[name=tamanho]');
+var outputTamanho = document.querySelector('[name=valorTamanho');
+
+function inputTamanho.oninput () {
+	outputTamanho.value = inputTamanho.value;
+}
+
+
